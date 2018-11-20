231e02aaa106bc69a76facb8257c8b68988f571c
Jackie <jackie@ghtinc.com>
2018-11-20 22:59:32 +0800

    return hard-code response

--- main.go
+++ main.go
@@ -1,28 +1,46 @@
 package main
 
 import (
-    //"encoding/json"
+    "encoding/json"
     "log"
     "net/http"
     "github.com/gorilla/mux"
     "fmt"
 )
 
-// our main function
+type Status struct {
+    ScanID      string  `json:"scanid,omitempty"`
+    QueueStatus string  `json:"queueStatus,omitempty"`
+    ConcisedLog string  `json:"concisedLog,omitempty"`
+}
+
+// main
 func main() {
     router := mux.NewRouter()
-    //router.HandleFunc("/search/{jid}", GetScanID).Methods("GET")
+    router.HandleFunc("/search/{jid}", GetScanID).Methods("GET")
     router.HandleFunc("/status/{scanid}", GetStatus).Methods("GET")
     router.HandleFunc("/scan/{scanid}", TriggerScan).Methods("GET")
     log.Fatal(http.ListenAndServe(":5000", router))
 }
 
-//func GetScanID(w http.ResponseWriter, r *http.Request) {}
-func GetStatus(w http.ResponseWriter, r *http.Request) {
+// use jid to get all scan id
+func GetScanID(w http.ResponseWriter, r *http.Request) {
     params := mux.Vars(r)
-    fmt.Println(params["scanid"])
+    fmt.Println("[INFO] Project ID: " + params["jid"])
 }
 
+// Get Queue & last build concised log
+func GetStatus(w http.ResponseWriter, r *http.Request) {
+    var currentStatus Status
+    currentStatus = Status{ScanID: "1001", QueueStatus: "1 job(s)",  ConcisedLog: "WTF"}
+
+    params := mux.Vars(r)
+    fmt.Println("[INFO] Scan ID: " + params["scanid"])
+    json.NewEncoder(w).Encode(currentStatus)
+}
+
+// Trigger scan
 func TriggerScan(w http.ResponseWriter, r *http.Request) {
-    fmt.Println(r)
+    params := mux.Vars(r)
+    fmt.Println("[INFO] Scan ID: " + params["scanid"])
 }
